6b08b43d8ee219c84c0466680e26b2482bc82294
Guilherme Moura Henrique <guilhermemoura.c710@gmail.com>
2023-03-23 21:11:28 -0300

    Organizando style.css

---
+++ main.js
@@ -0,0 +1,10 @@
+//var n1 = Number(window.prompt('Digite um número: '))
+//var n2 = Number(window.prompt('Digite oureo número número: '))
+//var s = n1 + n2
+//window.alert ('A soma é igual a ' + s)
+
+//document.ready = function(){//
+
+
+//};
+
